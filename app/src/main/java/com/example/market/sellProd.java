46b8d7330a2e7c682333fa99ad10cbc58296f3c0
Nawres99 <nawreshamdi99@gmail.com>
2021-02-10 10:38:44 -1100

    2

---
+++ app/src/main/java/com/example/market/sellProd.java
@@ -0,0 +1,14 @@
+package com.example.market;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sellProd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_prod);
+    }
+}
