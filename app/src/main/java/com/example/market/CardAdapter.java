b628362732ec127bf9b347396eb7eb5a19a30df2
SamyBchaalia <sami.benchaalia@sesame.com.tn>
2021-02-11 15:06:56 +0100

    fatma

---
+++ app/src/main/java/com/example/market/CardAdapter.java
@@ -0,0 +1,52 @@
+package com.example.market;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.MyViewHolder> {
+    ArrayList<Card> CardList;
+
+    public CardAdapter(ArrayList<Card> list){
+        this.CardList = list;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main,parent,false);
+        MyViewHolder cardAdapter = new MyViewHolder(view);
+        return cardAdapter;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.myText.setText(CardList.get(position).getTitle());
+        holder.myImage.setImageResource(R.drawable.soukelberka);
+    }
+
+    @Override
+    public int getItemCount() {
+        return CardList.size();
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView myText;
+        ImageView myImage;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            myText=itemView.findViewById(R.id.mycardtitle);
+            myImage=itemView.findViewById(R.id.mycardimage);
+
+        }
+    }
+}
--- app/src/main/java/com/example/market/MainActivity.java
+++ app/src/main/java/com/example/market/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.market;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ArrayList<Card> Mylist= new ArrayList<>();
+    CardAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.recyclerView);
+
+        Mylist.add(new Card("title 1",""));
+        Mylist.add(new Card("title 2",""));
+        Mylist.add(new Card("title 3",""));
+        adapter =new CardAdapter(Mylist);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
