694cc6c123e1d4b3a33d2c5ef0d9060f6e4f4cf6
Nawres99 <nawreshamdi99@gmail.com>
2021-02-15 09:59:44 -1100

    deux interfaces produits et shops

---
+++ app/src/main/java/MyAdapterprod.java
@@ -0,0 +1,64 @@
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.market.R;
+import com.example.market.myAdapter;
+
+class MyAdapterprod extends RecyclerView.Adapter<MyAdapterprod.MyViewHolder> {
+    String data3[], data4[],data5[];
+    int images1[];
+    Context context;
+
+    public MyAdapterprod(Context ct, String s3[], String s4[], String s5[],int img1[]) {
+        context = ct;
+        data3 = s3;
+        data4 = s4;
+        data5 = s5;
+        images1 = img1;
+    }
+
+    @NonNull
+    @Override
+    public MyAdapterprod.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater =LayoutInflater.from(context);
+        View view=inflater.inflate(R.layout.activity_item_seller,parent,false);
+        return new MyAdapterprod.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapterprod.MyViewHolder holder, int position) {
+        holder.myText3.setText(data3[position]);
+        holder.myText4.setText(data4[position]);
+        holder.myText5.setText(data5[position]);
+        holder.myImage.setImageResource(images1[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data3.length;
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView myText3,myText4,myText5;
+        ImageView myImage;
+        ConstraintLayout mainLayout;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            myText3=itemView.findViewById(R.id.productname);
+            myText4=itemView.findViewById(R.id.description2);
+            myText5=itemView.findViewById(R.id.price);
+
+            myImage=itemView.findViewById(R.id.balgha);
+
+        }
+    }
+}
--- app/src/main/java/com/example/market/Produits.java
+++ app/src/main/java/com/example/market/Produits.java
@@ -1,14 +1,43 @@
 package com.example.market;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 import android.os.Bundle;
 
 public class Produits extends AppCompatActivity {
+    RecyclerView recycler2;
+    private SnapHelper snapHelper;
+    String s1[] , s2[],s3[];
+    int images[]={R.drawable.balgha,R.drawable.balghaokhra};
+    ScaleCenterItemManager scaleCenterItemManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produits);
+
+
+
+            recycler2=findViewById(R.id.recycler1);
+
+
+            s1=getResources().getStringArray(R.array.products);
+            s2=getResources().getStringArray(R.array.description2);
+        s3=getResources().getStringArray(R.array.price);
+
+
+
+        myAdapter myAdapt=new myAdapter(this,s1,s2,images);
+            recycler2.setAdapter(myAdapt);
+            snapHelper = new LinearSnapHelper();
+            scaleCenterItemManager = new ScaleCenterItemManager(this, LinearLayoutManager.HORIZONTAL,false);
+            recycler2.setLayoutManager(new LinearLayoutManager(this));
+            recycler2.setLayoutManager(scaleCenterItemManager);
+            snapHelper.attachToRecyclerView(recycler2);
     }
 }
